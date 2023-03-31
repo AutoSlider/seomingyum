3a288c189fbc52a3b360d6ac0717bb561c7f692a
seomingyum <124328107+seomingyum@users.noreply.github.com>
2023-03-31 10:22:12 +0900

    Add files via upload

---
+++ common/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+app_name = 'common'
+
+urlpatterns = [
+    path('', views.main, name='main'),
+    path('signup', views.signup, name='signup'),
+    path('login', auth_views.LoginView.as_view(template_name='common/main.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('main_pop/', views.main_pop, name='main_pop'),
+    path('profile/', views.profile, name='profile'),
+    path('profile_pw/', views.profile_pw, name='profile_pw'),
+    path('produce/',views.produce,name='produce'),
+]
---
+++ common/views.py
@@ -0,0 +1,71 @@
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as auth_login
+from django.shortcuts import render, redirect
+from .forms import signupForm
+from .forms import UserProfileForm
+from django.contrib.auth.forms import PasswordChangeForm
+
+def main(request):
+    return render(request, 'common/main.html')
+
+def main_pop(request):
+    return render(request, 'common/main_pop.html')
+
+def board(request):
+    return render(request, 'pybo/pybo.html')
+
+def produce(request):
+    return render(request, 'common/produce.html')
+
+def signup(request):
+    if request.method == "POST":
+        form = signupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)  # 사용자 인증
+            auth_login(request, user)  # 로그인
+            if user:    # authenticate함수가 사용자 인증 처리를 성공하면 user는 True값 반환
+                return redirect('common:main_pop')    # main_pop 함수에서 랜더링하는 페이지가 팝업창 호출하는 기능 가지고 있음!
+        else:
+            return redirect('common:signup')
+    else:
+        form = signupForm()
+    return render(request, 'common/signup.html', {'form': form})
+
+def profile(request):
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('common:main')
+        else:
+            return redirect('common:profile')
+    else:
+        form = UserProfileForm(instance=request.user)
+        return render(request, 'common/profile.html', {'form': form})
+
+def profile_pw(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            form.save()
+            new_password1 = form.cleaned_data.get('new_password1')
+            user = authenticate(password=new_password1)  # 사용자 인증
+            auth_login(request, user)  # 로그인
+            return redirect('common:main')
+        else:
+            return redirect('common:profile_pw')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'common/profile_pw.html', {'form': form})
+
+
+
+
+
+
+
+
+
